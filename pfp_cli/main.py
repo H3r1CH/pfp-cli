818dfd317525e96c21bc55d685ef851325900a7e
H3r1CH <H3r1CH@protonmail.com>
2022-06-23 15:28:02 -0400

    Added version callback and command

--- pfp_cli/main.py
+++ pfp_cli/main.py
@@ -8,5 +8,20 @@ app.add_typer(enum.app, name="enum")
 app.add_typer(scan.app, name="scan")
 
 
+__version__ = "0.1.0"
+
+
+def version_callback(value: bool):
+    if value:
+        typer.echo(f"PFP-CLI Version: {__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(version: bool = typer.Option(None, "-v", "--version", 
+callback=version_callback, is_eager=True, help="Show current version of pfp-cli")):
+    return
+
+
 if __name__ == "__main__":
     app()
